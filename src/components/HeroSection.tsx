f92c3cdfeb965d637049a4c0084bc8032de9592c
Jamshadj <jamshadalij2004@gmail.com>
2025-09-15 17:25:58 +0530

    updated the images

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import img from "../../public/images/backgroundImage/Web_banner_1440x400 (1).jpg
 const HeroSection = () => {
   return (
     <div className='h-auto overflow-x-hidden'>
-      <Image  src={img} alt="alter" className='' />
+      <Image  src={img} alt="alter" className=' w-[100%]' />
     </div>
   )
 }
