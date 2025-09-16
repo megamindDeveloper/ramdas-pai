0ae7366001b24e263f94d202f901b869448a39f4
megamindDeveloper <developer.megamind@gmail.com>
2025-09-16 18:37:25 +0530

    uncomment

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,11 +11,11 @@ const HeroSection = () => {
       <div className='relative'>
    
       <Image  src={Mobileimg} alt="alter" className=' md:hidden w-[100%]' />
-      {/* <div className='md:hidden  absolute text-[#000000] top-0  space-y-2 left-0 p-5'>
+      <div className='md:hidden  absolute text-[#000000] top-0  space-y-2 left-0 p-5'>
       <h1 className='text-[34px]'>90 and forever</h1>
       <p className='text-base'>Wish you, the <span className='font-bold'>architect of moden Manipal-</span> a doyen of education and healthcare in india, a very happy birthyday </p>
       <p className='text-base'><span className='font-bold'>Dr.Ramdas M pai</span>, Padma Bhushan Awardee Chancellor,Manipal Academy of Higher Education(MAHE)</p>
-      </div> */}
+      </div>
       </div>
     </div>
   );
